06bf0a895712a4fff9d7c5df6004aff68b8e472b
Christoph Kassen <c.kassen@telekom.de>
2013-11-20 19:34:49 +0100

    Add data model and alembic migrations
    Add Node and Pacakge data model classes
    Use Alembic to migrate/upgrade the db structure
    Add configuration classes
    	Use sqlite for development purposes
    
    Refactor project structure:
    - Database code now in a seperate file
    - Models live in ponyexpress.models
    - manage.py in the root path for proper module imports

--- ponyexpress/__init__.py
+++ ponyexpress/__init__.py
@@ -2,9 +2,25 @@ from flask import Flask, jsonify
 
 from ponyexpress.api.v1.collector import collector
 from ponyexpress.api.v1.query import query
+
 from ponyexpress.api.exceptions import *
+from ponyexpress.config.configuration import *
+
+from ponyexpress.database import db
+from ponyexpress.models.node import Node
+from ponyexpress.models.package import Package
 
 app = Flask(__name__)
+
+# Load configuration
+#TODO: load configuration depending on environment
+app.config.from_object('ponyexpress.config.configuration.DevelopmentConfig')
+
+# Database
+db.init_app(app)
+
+#with app.app_context():
+#    db.create_all(app=app)
 
 # Register blueprints
 app.register_blueprint(collector)
