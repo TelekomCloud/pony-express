f257cc522cf9d2ad437e75cdad560d42ba85cc74
Dominik Richter <dominik.richter@gmail.com>
2013-11-21 12:09:07 +0100

    add test for packages found on one node
    Signed-off-by: Dominik Richter <dominik.richter@gmail.com>

--- ponyexpress/tests/ConsumerRestApi.py
+++ ponyexpress/tests/ConsumerRestApi.py
@@ -10,6 +10,21 @@ class BasicTestCaseV1(TestServerBase):
       'node1': {
         'id': 'my1.full.fqdn',
         'packages' : []
+      },
+      'node2': {
+        'id': 'my2.full.fqdn',
+        'packages': [
+          {
+            "name": "accountsservice",
+            "uri": "http://us.archive.ubuntu.com/ubuntu/pool/main/a/accountsservice/accountsservice_0.6.15-2ubuntu9_amd64.deb",
+            "summary": "query and manipulate user account information",
+            "version": "0.6.15-2ubuntu9",
+            "architecture": "amd64",
+            "sha256": "5d8e40ce35ea30f621573d40b17dcd21e3b974f2dd5e096c6c10701af8cdc5d0",
+            "provider": "apt",
+            "archive": "precise"
+          }
+        ]
       }
     }
 
@@ -36,3 +51,16 @@ class BasicTestCaseV1(TestServerBase):
         j = self.get_json('/v1/node/'+self.DATA['node1']['id'])
         eq_( type( j ), dict )
         eq_( r['packages'], [] )
+
+    def testRequestNodePackages(self):
+        self.addNode(self.DATA['node2'])
+        j = self.get_json('/v1/node/'+self.DATA['node2']['id'])
+        eq_( type( j ), dict )
+
+        ps = r['packages']
+        eq_( type( ps ), list )
+        eq_( len( ps ), 1 )
+
+        package = ps[0]
+        eq_( type( package ), dict )
+        eq_( package['id'], self.DATA['node2']['packages'][0]['sha256'] )
