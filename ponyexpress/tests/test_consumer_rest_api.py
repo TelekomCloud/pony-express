e322415613c415f7914e803521ce37dea824dbf3
Dominik Richter <dominik.richter@gmail.com>
2013-11-25 13:25:49 +0100

    improvement: make sure every package has an id field
    fill the id field with sha256 for apt packages
    
    Signed-off-by: Dominik Richter <dominik.richter@gmail.com>

--- ponyexpress/tests/test_consumer_rest_api.py
+++ ponyexpress/tests/test_consumer_rest_api.py
@@ -20,7 +20,7 @@ class BasicTestCaseV1(TestServerBase):
                     "summary": "query and manipulate user account information",
                     "version": "0.6.15-2ubuntu9",
                     "architecture": "amd64",
-                    "sha256": "5d8e40ce35ea30f621573d40b17dcd21e3b974f2dd5e096c6c10701af8cdc5d0",
+                    "id": "5d8e40ce35ea30f621573d40b17dcd21e3b974f2dd5e096c6c10701af8cdc5d0",
                     "provider": "apt",
                     "archive": "precise"
                 }
@@ -63,7 +63,7 @@ class BasicTestCaseV1(TestServerBase):
 
         package = ps[0]
         eq_(type(package), dict)
-        eq_(package['id'], self.DATA['node2']['packages'][0]['sha256'])
+        eq_(package['id'], self.DATA['node2']['packages'][0]['id'])
 
     def testRequestPackages(self):
         self.addNode(self.DATA['node2'])
@@ -71,18 +71,18 @@ class BasicTestCaseV1(TestServerBase):
         j = self.get_json('/v1/packages')
         eq_(type(j), list)
         eq_(len(j), 1)
-        eq_(j[0]["id"], p["sha256"])
+        eq_(j[0]["id"], p["id"])
 
     def testRequestPackageInfo(self):
         self.addNode(self.DATA['node2'])
         p = self.DATA['node2']['packages'][0]
-        j = self.get_json('/v1/package/' + p['sha256'])
+        j = self.get_json('/v1/package/' + p['id'])
         eq_(type(j), dict)
         eq_(j["name"], p["name"])
         eq_(j["uri"], p["uri"])
         eq_(j["summary"], p["summary"])
         eq_(j["version"], p["version"])
         eq_(j["architecture"], p["architecture"])
-        eq_(j["sha256"], p["sha256"])
+        eq_(j["id"], p["id"])
         eq_(j["provider"], p["provider"])
         eq_(j["archive"], p["archive"])
