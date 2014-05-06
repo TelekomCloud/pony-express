745169fb054eba1f9b42612cf65eb47f44b0348f
Christoph Kassen <c.kassen@telekom.de>
2014-05-06 13:31:18 +0200

    Add missing attributes to pagination class
    Add per_page, next_num and prev_num to Pagination class.
    
    Fixes #41
    
    Signed-off-by: Christoph Kassen <c.kassen@telekom.de>

--- ponyexpress/api/lib/helpers/pagination.py
+++ ponyexpress/api/lib/helpers/pagination.py
@@ -1,3 +1,6 @@
+# Custom pagination class
+# Taken from http://flask.pocoo.org/snippets/44/
+
 from math import ceil
 
 
@@ -7,7 +10,7 @@ class Pagination(object):
 
     def __init__(self, page, per_page, total_count):
         self.page = page
-        self.per_page = per_page
+        self.obj_per_page = per_page
         self.total_count = total_count
 
     #@property
@@ -15,8 +18,22 @@ class Pagination(object):
     #    return self.items
 
     @property
+    def per_page(self):
+        return self.obj_per_page
+
+    @property
     def pages(self):
         return int(ceil(self.total_count / float(self.per_page)))
+
+    @property
+    def next_num(self):
+        p = self.page + 1
+        return p if p <= self.pages else self.page
+
+    @property
+    def prev_num(self):
+        p = self.page - 1
+        return p if p >= 1 else self.page
 
     @property
     def has_prev(self):
